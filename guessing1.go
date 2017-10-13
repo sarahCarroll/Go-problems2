28af1913c0c618d6c40a651ce45f1175a6fda592
sarahc123 <g00330821@gmit.ie>
2017-10-13 18:36:13 +0100

    Comments added

--- guessing1.go
+++ guessing1.go
@@ -1,15 +1,21 @@
+//Author- Sarah Carroll
+// https://golang.org/doc/articles/wiki/
+
 package main
 
+//To use the net/http package, it must be imported
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
+//The main function begins with a call to http.HandleFunc, which tells the http package to handle all requests to the web root ("/") with handler.
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "Guessing Game! ")
+	fmt.Fprint(w, "Guessing Game! ")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	//call handler function
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
